2ee2f3efe881ea823876005f6b6a51781fc70972
mertalev <101130780+mertalev@users.noreply.github.com>
2023-12-13 21:30:13 -0500

    use fixed libvips

--- server/bin/build-libraw.sh
+++ server/bin/build-libraw.sh
@@ -2,18 +2,12 @@
 
 set -e
 
-LOCK=$(jq -c '.packages[] | select(.name == "libraw")' build-lock.json)
-LIBRAW_VERSION=${LIBRAW_VERSION:=$(echo $LOCK | jq -r '.version')}
-LIBRAW_SHA256=${LIBRAW_SHA256:=$(echo $LOCK | jq -r '.sha256')}
+: "${LIBRAW_REVISION:=$(jq -cr '.sources[] | select(.name == "libraw").revision' build-lock.json)}"
 
-echo "$LIBRAW_SHA256  $LIBRAW_VERSION.tar.gz" > libraw.sha256
-mkdir -p libraw
-wget -nv https://github.com/libraw/libraw/archive/${LIBRAW_VERSION}.tar.gz
-sha256sum -c libraw.sha256
-tar -xvf ${LIBRAW_VERSION}.tar.gz -C libraw --strip-components=1
-rm ${LIBRAW_VERSION}.tar.gz
-rm libraw.sha256
+git clone https://github.com/libraw/libraw.git
 cd libraw
+git reset --hard $LIBVIPS_REVISION
+
 autoreconf --install
 ./configure
 make -j$(nproc)
--- server/bin/build-libvips.sh
+++ server/bin/build-libvips.sh
@@ -2,21 +2,14 @@
 
 set -e
 
-LOCK=$(jq -c '.packages[] | select(.name == "libvips")' build-lock.json)
-LIBVIPS_VERSION=${LIBVIPS_VERSION:=$(echo $LOCK | jq -r '.version')}
-LIBVIPS_SHA256=${LIBVIPS_SHA256:=$(echo $LOCK | jq -r '.sha256')}
+: "${LIBVIPS_REVISION:=$(jq -cr '.sources[] | select(.name == "libvips").revision' build-lock.json)}"
 
-echo "$LIBVIPS_SHA256  vips-$LIBVIPS_VERSION.tar.xz" > libvips.sha256
-mkdir -p libvips
-wget -nv https://github.com/libvips/libvips/releases/download/v${LIBVIPS_VERSION}/vips-${LIBVIPS_VERSION}.tar.xz
-sha256sum -c libvips.sha256
-tar -xvf vips-${LIBVIPS_VERSION}.tar.xz -C libvips --strip-components=1
-rm vips-${LIBVIPS_VERSION}.tar.xz
-rm libvips.sha256
+git clone https://github.com/libvips/libvips.git
 cd libvips
+git reset --hard $LIBVIPS_REVISION
+
 meson setup build --buildtype=release --libdir=lib -Dintrospection=disabled -Dtiff=disabled
 cd build
-# ninja test  # tests set concurrency too high for arm/v7
 ninja install
 cd .. && rm -rf libvips
 ldconfig /usr/local/lib
