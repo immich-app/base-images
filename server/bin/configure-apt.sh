8201f19c289109e67e7de681dfa8c58282adb48b
martin <74269598+martabal@users.noreply.github.com>
2024-09-05 16:59:28 +0200

    feat: install `libio-compress-brotli-perl` from the trixie repo instead of bulding it (#105)

--- server/bin/configure-apt.sh
+++ server/bin/configure-apt.sh
@@ -3,11 +3,16 @@
 set -e
 
 sed -i -e's/ main/ main contrib non-free non-free-firmware/g' /etc/apt/sources.list.d/debian.sources
-sed -i -e's/ bookworm-updates/ bookworm-updates sid/g' /etc/apt/sources.list.d/debian.sources
+sed -i -e's/ bookworm-updates/ bookworm-updates testing sid/g' /etc/apt/sources.list.d/debian.sources
 
 # default priority is 500, so we set unstable to 450 to prefer stable packages
 cat > /etc/apt/preferences.d/preferences << EOL
 Package: *
 Pin: release a=unstable
 Pin-Priority: 450
+
+Package: *
+Pin: release a=testing
+Pin-Priority: 450
 EOL
+
