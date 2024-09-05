f1715b3dd2140f542d77afadbdd911d9116df843
martabal <74269598+martabal@users.noreply.github.com>
2024-09-05 12:42:55 +0200

    fix: rename trixie to testing

--- server/bin/configure-apt.sh
+++ server/bin/configure-apt.sh
@@ -3,7 +3,7 @@
 set -e
 
 sed -i -e's/ main/ main contrib non-free non-free-firmware/g' /etc/apt/sources.list.d/debian.sources
-sed -i -e's/ bookworm-updates/ bookworm-updates trixie sid/g' /etc/apt/sources.list.d/debian.sources
+sed -i -e's/ bookworm-updates/ bookworm-updates testing sid/g' /etc/apt/sources.list.d/debian.sources
 
 # default priority is 500, so we set unstable to 450 to prefer stable packages
 cat > /etc/apt/preferences.d/preferences << EOL
@@ -12,7 +12,7 @@ Pin: release a=unstable
 Pin-Priority: 450
 
 Package: *
-Pin: release n=trixie
+Pin: release a=testing
 Pin-Priority: 450
 EOL
 
