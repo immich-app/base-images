406c2c6706b2f73ec043cb0f0d6b7216b9b42db1
Cédric Laubacher <cedric@laubacher.io>
2024-10-08 02:01:22 +0200

    feat: build libjxl v0.11.0 (#125)
    * feat: build libjxl v0.11.0
    
    * fixes
    
    * remove liborc in favor of libhwy
    
    * fix comment
    
    ---------
    
    Co-authored-by: mertalev <101130780+mertalev@users.noreply.github.com>

---
+++ server/bin/build-libjxl.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+: "${LIBJXL_REVISION:=$(jq -cr '.sources[] | select(.name == "libjxl").revision' build-lock.json)}"
+
+git clone https://github.com/libjxl/libjxl.git
+cd libjxl
+git reset --hard $LIBJXL_REVISION
+git submodule update --init --recursive --depth 1 --recommend-shallow
+
+mkdir build
+cd build
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_TESTING=OFF \
+  -DJPEGXL_ENABLE_DOXYGEN=OFF \
+  -DJPEGXL_ENABLE_MANPAGES=OFF \
+  -DJPEGXL_ENABLE_PLUGIN_GIMP210=OFF \
+  -DJPEGXL_ENABLE_BENCHMARK=OFF \
+  -DJPEGXL_ENABLE_EXAMPLES=OFF \
+  -DJPEGXL_FORCE_SYSTEM_BROTLI=ON \
+  -DJPEGXL_FORCE_SYSTEM_HWY=ON \
+  -DJPEGXL_ENABLE_JPEGLI=ON \  
+  -DJPEGXL_ENABLE_JPEGLI_LIBJPEG=ON \  
+  -DJPEGXL_INSTALL_JPEGLI_LIBJPEG=ON \  
+  -DJPEGXL_ENABLE_AVX512=ON \  
+  -DJPEGXL_ENABLE_AVX512_ZEN4=ON \
+  -DJPEGXL_ENABLE_PLUGINS=ON \
+  ..
+cmake --build . -- -j$(nproc)
+cmake --install .
+cd ../.. && rm -rf libjxl
+ldconfig /usr/local/lib
