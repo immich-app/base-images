aceffd4c8bb9c4d1685786a31813db736b99ea33
Zack Pollard <zackpollard@ymail.com>
2025-05-21 15:24:08 +0100

    fix: only create config if file doesn't already exist on disk

--- postgres/immich-docker-entrypoint.sh
+++ postgres/immich-docker-entrypoint.sh
@@ -5,7 +5,7 @@
 case "${DB_STORAGE_TYPE^^}" in
   SSD|HDD)
     echo "Using ${DB_STORAGE_TYPE^^} storage"
-    cp "/etc/postgresql/postgresql.${DB_STORAGE_TYPE,,}.conf" /etc/postgresql/postgresql.conf
+    cp -n "/etc/postgresql/postgresql.${DB_STORAGE_TYPE,,}.conf" /etc/postgresql/postgresql.conf
     ;;
   *)
     echo "Error: DB_STORAGE_TYPE must be set to 'SSD' or 'HDD'" >&2
