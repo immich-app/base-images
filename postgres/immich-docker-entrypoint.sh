1f8398155909759fdc16b2036848fb548dcf7e31
Zack Pollard <zackpollard@ymail.com>
2025-05-22 17:02:02 +0100

    feat: support PGDATA postgres variable (#219)

--- postgres/immich-docker-entrypoint.sh
+++ postgres/immich-docker-entrypoint.sh
@@ -6,6 +6,7 @@ case "${DB_STORAGE_TYPE^^}" in
   SSD|HDD)
     echo "Using ${DB_STORAGE_TYPE^^} storage"
     cp -n "/etc/postgresql/postgresql.${DB_STORAGE_TYPE,,}.conf" /etc/postgresql/postgresql.conf
+    sed -i "s@##PGDATA@$PGDATA@" /etc/postgresql/postgresql.conf; \
     ;;
   *)
     echo "Error: DB_STORAGE_TYPE must be set to 'SSD' or 'HDD'" >&2
