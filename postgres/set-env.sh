d9a68042133252cb21e6867df69ef083c6b7670f
Zack Pollard <zackpollard@ymail.com>
2025-05-28 15:15:21 +0100

    feat: handle docker secrets in postgres healthcheck

---
+++ postgres/set-env.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+read_env_or_file() {
+    local var_name="$1"
+    local file_var_name="${var_name}_FILE"
+
+    if [[ -n "${!file_var_name}" && -f "${!file_var_name}" && -s "${!file_var_name}" ]]; then
+        cat "${!file_var_name}"
+    else
+        echo "${!var_name}"
+    fi
+}
+
+: "${POSTGRES_USER_FILE:=${DB_USERNAME_FILE}}"
+: "${POSTGRES_PASSWORD_FILE:=${DB_PASSWORD_FILE}}"
+: "${POSTGRES_DB_FILE:=${DB_DATABASE_NAME_FILE}}"
+
+POSTGRES_DB=$(read_env_or_file "POSTGRES_DB")
+POSTGRES_USER=$(read_env_or_file "POSTGRES_USER")
+POSTGRES_PASSWORD=$(read_env_or_file "POSTGRES_PASSWORD")
+
+: "${POSTGRES_USER:=${DB_USERNAME}}"
+: "${POSTGRES_PASSWORD:=${DB_PASSWORD}}"
+: "${POSTGRES_DB:=${DB_DATABASE_NAME}}"
+
+export POSTGRES_DB POSTGRES_USER POSTGRES_PASSWORD
