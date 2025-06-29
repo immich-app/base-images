6a1982bb7f2a8c8893214f55cbc3419496617d98
Min Idzelis <min123@gmail.com>
2025-06-29 17:21:54 +0000

    feat: add AOM/SVT-AV1 to libheif

--- server/sources/libheif.sh
+++ server/sources/libheif.sh
@@ -4,6 +4,15 @@ set -e
 
 : "${LIBHEIF_REVISION:=$(jq -cr '.revision' libheif.json)}"
 
+# Build and install SVT-AV1 (not available in Debian repos)
+git clone https://gitlab.com/AOMediaCodec/SVT-AV1.git
+cd SVT-AV1
+cd Build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
+make -j$(nproc) install
+cd ../..
+rm -rf SVT-AV1
+
 git clone https://github.com/strukturag/libheif.git
 cd libheif
 git reset --hard "$LIBHEIF_REVISION"
@@ -15,11 +24,12 @@ cmake --preset=release-noplugins \
     -DENABLE_PARALLEL_TILE_DECODING=ON \
     -DWITH_LIBSHARPYUV=ON \
     -DWITH_LIBDE265=ON \
-    -DWITH_AOM_DECODER=OFF \
-    -DWITH_AOM_ENCODER=OFF \
+    -DWITH_AOM_DECODER=ON \
+    -DWITH_AOM_ENCODER=ON \
+    -DWITH_SvtEnc=ON \
     -DWITH_X265=OFF \
     -DWITH_EXAMPLES=OFF \
     ..
-make install
+make -j$(nproc) install
 cd ../.. && rm -rf libheif
 ldconfig /usr/local/lib
