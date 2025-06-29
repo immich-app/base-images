9fb10077f53bd35da009e926604d6cb2dceb547e
Min Idzelis <min123@gmail.com>
2025-06-29 17:42:03 +0000

    Use aom, not svt1

--- server/sources/libheif.sh
+++ server/sources/libheif.sh
@@ -4,15 +4,6 @@ set -e
 
 : "${LIBHEIF_REVISION:=$(jq -cr '.revision' libheif.json)}"
 
-# Build and install SVT-AV1 (not available in Debian repos)
-git clone https://gitlab.com/AOMediaCodec/SVT-AV1.git
-cd SVT-AV1
-cd Build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
-make -j$(nproc) install
-cd ../..
-rm -rf SVT-AV1
-
 git clone https://github.com/strukturag/libheif.git
 cd libheif
 git reset --hard "$LIBHEIF_REVISION"
@@ -26,7 +17,6 @@ cmake --preset=release-noplugins \
     -DWITH_LIBDE265=ON \
     -DWITH_AOM_DECODER=ON \
     -DWITH_AOM_ENCODER=ON \
-    -DWITH_SvtEnc=ON \
     -DWITH_X265=OFF \
     -DWITH_EXAMPLES=OFF \
     ..
