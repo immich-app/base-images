0d9a380b3d0ff354f734126a5623441ad717b4c7
Mert <101130780+mertalev@users.noreply.github.com>
2025-10-01 01:31:21 -0400

    fix(server): memory leak in allocator (#277)
    use newer libmimalloc
    
    configure before install
    
    remove sid
    
    update comment

--- server/configure-apt.sh
+++ server/configure-apt.sh
@@ -4,20 +4,19 @@ set -e
 
 sed -i -e's/ main/ main contrib non-free non-free-firmware/g' /etc/apt/sources.list.d/debian.sources
 
-# Note: the following lines are commented out because mixing trixie with unstable/testing causes package
-# resolution conflicts due to the time64 migration. Trixie rebuilt all packages with a 64 bit time_t, but
-# they left the 32 bit time version in place. The 64 bit time version has t64 sufix in the package name.
+# Note: does not add `sid` since mixing it with trixie can cause package resolution conflicts
+# due to the time64 migration. Trixie rebuilt all packages with a 64 bit time_t, but not all
+# packages in unstable have migrated yet. The 64 bit time version has t64 suffix in the package name.
 # More info: https://lwn.net/Articles/938149/ https://wiki.debian.org/ReleaseGoals/64bit-time
-#
-# sed -i -e"s/ ${DEBIAN_RELEASE}-updates/ ${DEBIAN_RELEASE}-updates testing sid/g" /etc/apt/sources.list.d/debian.sources
-#
+sed -i -e"s/ ${DEBIAN_RELEASE}-updates/ ${DEBIAN_RELEASE}-updates testing/g" /etc/apt/sources.list.d/debian.sources
+
 # # default priority is 500, so we set unstable to 450 to prefer stable packages
-# cat > /etc/apt/preferences.d/preferences << EOL
-# Package: *
-# Pin: release a=unstable
-# Pin-Priority: 450
-#
-# Package: *
-# Pin: release a=testing
-# Pin-Priority: 450
-# EOL
+cat > /etc/apt/preferences.d/preferences << EOL
+Package: *
+Pin: release a=unstable
+Pin-Priority: 450
+
+Package: *
+Pin: release a=testing
+Pin-Priority: 450
+EOL
