cf04e867604c0a66c68c9d42173d032570678352
Savely Krasovsky <savely@krasovs.ky>
2026-02-28 14:19:24 +0100

    feat: add support for the latest intel media driver

---
+++ server/sources/intel-media-driver.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+: "${INTEL_MEDIA_DRIVER_REVISION:=$(jq -cr '.revision' intel-media-driver.json)}"
+
+git clone https://github.com/intel/media-driver.git
+cd media-driver && git reset --hard "${INTEL_MEDIA_DRIVER_REVISION}" && cd ..
+mkdir build_media
+cd build_media
+export ENABLE_PRODUCTION_KMD=ON
+cmake \
+  -D DINSTALL_DRIVER_SYSCONF=OFF \
+  -D ENABLE_KERNELS=ON \
+  -D ENABLE_NONFREE_KERNELS=ON \
+  -D BUILD_CMRTLIB=OFF \
+  -D MEDIA_BUILD_FATAL_WARNINGS=OFF \
+  ../media-driver
+make -j"$(nproc)"
+make install
+cd ..
+rm -rf build_media media-driver
