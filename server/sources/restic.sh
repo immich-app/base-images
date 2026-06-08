07d24224d289b8521b43243bb5945a1a3fce235c
izzy <me@insrt.uk>
2026-06-08 12:29:07 +0200

    chore: pin go toolchain as Docker image

--- server/sources/restic.sh
+++ server/sources/restic.sh
@@ -2,18 +2,7 @@
 
 set -e
 
-export TARGETARCH=${TARGETARCH:=$(dpkg --print-architecture)}
 : "${RESTIC_REVISION:=$(jq -cr '.revision' restic.json)}"
-: "${GO_VERSION:=$(jq -cr '.go.version' restic.json)}"
-: "${GO_SHA256:=$(jq -cr '.go.sha256[$ENV.TARGETARCH]' restic.json)}"
-
-GO_TARBALL="go${GO_VERSION}.linux-${TARGETARCH}.tar.gz"
-wget -nv "https://go.dev/dl/${GO_TARBALL}"
-echo "${GO_SHA256}  ${GO_TARBALL}" | sha256sum -c -
-tar -C /usr/local -xzf "${GO_TARBALL}"
-rm "${GO_TARBALL}"
-export PATH="/usr/local/go/bin:${PATH}"
-export GOTOOLCHAIN=local CGO_ENABLED=0
 
 git clone https://github.com/restic/restic.git
 cd restic
