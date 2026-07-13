794ab86ff244e58c0942c6136ea77fa14218e4a9
mertalev <101130780+mertalev@users.noreply.github.com>
2026-07-13 16:17:18 -0400

    fix loader priority

--- server/sources/libvips.sh
+++ server/sources/libvips.sh
@@ -7,6 +7,7 @@ set -e
 git clone https://github.com/libvips/libvips.git
 cd libvips
 git reset --hard "$LIBVIPS_REVISION"
+git apply ../libvips-patches/0001-put-other-loaders-ahead-of-dcrawload.patch
 
 meson setup build --buildtype=release --libdir=lib -Dintrospection=disabled -Dtiff=disabled
 cd build
